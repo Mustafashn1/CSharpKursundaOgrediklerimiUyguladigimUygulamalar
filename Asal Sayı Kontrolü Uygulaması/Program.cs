43a8b0bf5c72fda448fa2523e940054f37aa10c1
Mustafa <mustafashn.1@outlook.com>
2024-11-01 22:15:50 +0300

    Veri tipleri, şart blokları, metodlar, döngüler kullanılarak bir Asal sayı bulma uygulaması yaptım

---
+++ Asal Sayı Kontrolü Uygulaması/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asal_Sayı_Kontrolü_Uygulaması
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Bir Sayi Giriniz...");
+                if (int.TryParse(Console.ReadLine(), out int sayi))
+                {
+                    if (AsalMi(sayi))
+                        Console.WriteLine($"{sayi} asal bir sayıdır.");
+                    else
+                        Console.WriteLine($"{sayi} asal değildir.");
+                }
+                else
+                {
+                    Console.WriteLine("Geçersiz bir sayı girdiniz. Lütfen tekrar deneyin.");
+                }
+
+            }
+        }
+
+
+
+
+        static bool AsalMi(int sayi)
+        {
+            if (sayi <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i < sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+            
+        }
+    }
+}
---
+++ Asal Sayı Kontrolü Uygulaması/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Asal Sayı Kontrolü Uygulaması")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Asal Sayı Kontrolü Uygulaması")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3c12c128-359c-44c9-9d5f-d86a7c5c49de")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
